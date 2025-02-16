e69106e3c066b101e03fa0ea0fd067a9f8922bf0
yufeng.tao <yufeng.tao@workday.com>
2025-02-16 19:33:08 +0000

    add a_13.js script for Lab1 A.13 question

---
+++ lab1/a_13.js
@@ -0,0 +1,16 @@
+var zlib = require('zlib')
+
+var compressedStr1 = "eJzzyc9Lyc8DAAgpAms="
+var compressedStr2 = "eJxzSi3KycwDAAfXAl0="
+var compressedStr3 = "eJzzSy1XiMwvygYADKUC8A=="
+
+function decompress(err, buf) {
+    if (buf == undefined)
+        console.log("The value to decompress is in incorrect format for processing Base64!")
+    else
+        console.log("Decompressed string is: " + buf.toString("utf8"))
+}
+
+zlib.inflate(new Buffer.from(compressedStr1, 'base64'), decompress)
+zlib.inflate(new Buffer.from(compressedStr2, 'base64'), decompress)
+zlib.inflate(new Buffer.from(compressedStr3, 'base64'), decompress)
