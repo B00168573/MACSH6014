30f485df94b4697a7a5a96c502f7e19c7247fa76
yufeng.tao <yufeng.tao@workday.com>
2025-02-16 22:35:53 +0000

    update C2.py script

--- lab1/C2.py
+++ lab1/C2.py
@@ -10,13 +10,13 @@ def checkIsPrime(num):
     else:
         return False
 
-def cipherFunc(baseNum, powerNum, modNum):
+def cipherFunc(baseNum, powerNum, modNum, expectedNum):
     checkPrimeResult = checkIsPrime(modNum)
     finalResult = pow(baseNum, powerNum) % modNum
     print("Is number \t", modNum, " a prime number? \t", checkPrimeResult)
     print("Result of (\t ", baseNum, " to the power of \t", powerNum, ") then mod by \t", modNum, " is \t", finalResult)
+    print("Is the result of (\t", baseNum, " to the power of \t", powerNum, ") then mod by \t", modNum, " equals to \t", expectedNum, "? \t", finalResult == expectedNum)
     return finalResult
 
-finalResult = cipherFunc(8, 5, 269)
-print("Is the result of (8 to the power of 5) equal to 219? \t", finalResult == 219)
+finalResult = cipherFunc(8, 5, 269, 219)
 
