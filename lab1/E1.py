80a2868ad33532ad486b8b98c7b4961e686b9f68
yufeng.tao <yufeng.tao@workday.com>
2025-02-17 04:55:13 +0000

    add E1.py script for Lab1 E1 question

---
+++ lab1/E1.py
@@ -0,0 +1,10 @@
+
+def linearNumGenerator(a, seed, c, m, count):
+    vals = [seed]
+    for n in range(0, count):
+        result = (a * seed + c) % m
+        vals.append(result)
+        seed = result
+    return vals
+
+print("The generated sequence numbers are: \t", linearNumGenerator(21, 35, 31, 100, 5))
